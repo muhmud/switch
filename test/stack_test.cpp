159639a2f26281c6bb36a09341c5313278a27d81
Muhmud Ahmad <muhmud.ahmad@toptal.com>
2023-10-01 05:03:17 +0300

    fix for tests

--- test/stack_test.cpp
+++ test/stack_test.cpp
@@ -1,9 +1,12 @@
+extern "C" {
 #include "../src/stack.h"
+}
+
 #include <gtest/gtest.h>
 
 TEST(StackTest, new_stack_item) {
   const char *id = "abc";
   struct StackItem *item = new_stack_item(id);
 
-  EXPECT_EQ(item->id, id);
+  EXPECT_STREQ(item->id, id);
 }
